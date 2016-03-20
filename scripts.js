578a0cba009e17197ab9af72d5a99a73a461a774
Erick Suarez <ssping5@gmail.com>
2016-03-20 15:43:00 -0500

    Correjido

--- scripts.js
+++ scripts.js
@@ -2,5 +2,6 @@ console.log("Inicio de Programa");
 alert("HOLA JAVASCRIPT");
 document.write ("<h1>hola javascript !!!</h1>");
 alert("gracias por visitar");
-
+alert("vales");
+console.log("Fin de Programa");
 
